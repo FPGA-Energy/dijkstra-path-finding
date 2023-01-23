63bbb07df9c6caf31baa9f95a7f3cb470720101f
Maja Hanne Kirkeby <MHKirkeby@users.noreply.github.com>
2023-01-23 14:14:38 +0100

    Update test4RasperryPiC.sh
    creates a CSV file and the logfile has a timestamp in its name.

--- src/scripts/test4RasperryPiC.sh
+++ src/scripts/test4RasperryPiC.sh
@@ -1,62 +1,66 @@
 	#!/bin/bash
 
 	# only real time
+	T1="$(date +%s%N)"
 	TIMEFORMAT=%R
-	LOGFILE="results/runtime.log"
+	LOGFILE="../../results/Raspberry Pi/runtime_${T1}.log"
 	REP1=40
 	REP2=40
 	REP3=20
 
-	benchcmd() {
-
-			echo "$@;"  >> $LOGFILE
+	benchcmd() {	
+			#echo "$@;"  >> $LOGFILE
 			T="$(date +%s%N)"
 			eval "$@"
 			T="$(($(date +%s%N)-T))"
 			T=$((T/1000000))
-			echo "${T};::" >> $LOGFILE
-			#sleep 1
+			echo "$@;${T}" >>$LOGFILE
+			sleep 5
 
 	}
 
-	run_files() {
 
+	run_files() {
+		echo $LOGFILE
+		echo "[$(date)]" >> $LOGFILE				
+		
 		for file in $1
 		do 
-			echo $file 
-			echo "[$(date)]" >> $LOGFILE		
-			for j1 in 1 2 3 4 5 6 7 8 9 10
+            #            echo $LOGFILE
+			# echo "[$(date)]" >> $LOGFILE		
+			for j1 in 1 2 # 3 4 5 6 7 8 9 10
 			do	
 				echo "$j1; "
 
-				for j2 in 1 2 3 4
+				for j2 in 1 #2 3 4
 				do	
-					for i in 1 2 3 4 5 6 7 8 9 10
+					for i in 1 # 2 3 4 5 6 7 8 9 10
 					do
-						echo "$j1*$j2*$i; " >> $LOGFILE
-						benchcmd $file 
+#						echo "$j1*$j2*$i; " >> $LOGFILE
+						benchcmd $file
 
 					done
 				done
 
 			done
-			echo "[$(date)]" >> $LOGFILE
-			#sleep 20
+#			echo "[$(date)]" >> $LOGFILE
+			sleep 10
 
 		done
 
 	}
 
+	
+#	rm -f  $LOGFILE
 
-	rm -f $LOGFILE
-
-	pwd >> $LOGFILE
-	echo "[$(date)]" >> $LOGFILE
-
-	run_files "../build/*K-random_*.heapsort.out"
-	run_files "../build/*K-sorted_*.heapsort.out"
-	run_files "../build/*K-reverse-sorted_*.heapsort.out"
+#	pwd >>  "$LOGFILE$(file).log"
+#	echo "[$(date)]" >>  "$LOGFILE$(file).log"
+	
+	run_files "../../executables/*"
+	# run_files "../build/*K-random_*.heapsort.out"
+	# run_files "../build/*K-sorted_*.heapsort.out"
+	# run_files "../build/*K-reverse-sorted_*.heapsort.out"
 
 
 	echo "done"
-	cat $LOGFILE
+	cat  $LOGFILE
